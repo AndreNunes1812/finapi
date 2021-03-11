cdaa4f913b3ea7338fde970d6141cb4705c6ebbe
AndreNunes1812 <andre.nunes020770@gmail.com>
2021-03-11 19:13:32 +0100

    criação do recurso POST e do arquivo README

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
 const express = require("express");
+const {v4: uuidv4 } = require("uuid");
 
 const app = express();
+app.use(express.json());
+
+const customers = [];
+
+/**
+ * CPF - string 
+ * name - string
+ * id - uuid
+ * statement []
+ */
+
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+
+  const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
+
+  if(customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already exists!"});
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: []
+  })
+
+  return response.status(201).send();
+
+})
 
 app.listen(3333)
